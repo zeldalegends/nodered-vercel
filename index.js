bd55ce74d0f971c507c5b1075762f931c193e0c1
zeldalegends <73659648+zeldalegends@users.noreply.github.com>
2021-12-18 21:08:12 +0000

    Updated structure

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-console.log("Node-RED on line (embedded version)");
-console.log(Object.values(require('os')));
-console.log(Object.values(process));
-
-var http = require('http');
-var express = require("express");
-var RED = require("node-red");
+const http = require('http');
+const express = require("express");
+const RED = require("node-red");
 
 var app = express();
-app.use("/",express.static("public"));
+app.use("/", express.static("public"));
 var server = http.createServer(app);
 
 var settings = {
-  httpAdminRoot:"/",
-  httpNodeRoot: "/",
-  userDir:"./",
-  flowFile: "flows.json",
-  credentialSecret: false,
-  editorTheme: {
-    projects: {
-      enabled: false,
-    },
-    codeEditor: {
-      lib: "monaco",
-      options: {
-        theme: "vs"
-      }
-    }
-  },
+	httpAdminRoot: "/editor",
+	httpNodeRoot: "/",
+	userDir: ".",
+	flowFile: "flows.json",
+	credentialSecret: false,
+	editorTheme: {
+		projects: {
+			enabled: false,
+		},
+		codeEditor: {
+			lib: "monaco",
+			options: {
+				theme: "vs"
+			}
+		}
+	},
 };
 
 RED.init(server, settings);
@@ -34,3 +30,25 @@ app.use(settings.httpAdminRoot, RED.httpAdmin);
 app.use(settings.httpNodeRoot, RED.httpNode);
 server.listen(8000);
 RED.start();
+
+/** const https = require('https');
+const options = 'https://nodered.zeldalegends.repl.co';
+
+//const options = {hostname: 'nodered.zeldalegends.repl.co',	port: 80,	path: '/test',	headers: {'User-Agent': 'Mozilla/5.0'}};
+
+setInterval(() => {
+	https.request(options, res => {
+		res.on('data', d => {
+			//process.stdout.write(d);
+			console.log(` (statusCode: ${res.statusCode})`);
+		})
+	})
+		.on('error', error => {
+			console.error(error)
+		})
+		.end();
+}, 60000);
+
+//console.log(process.version);
+//console.log(Object.values(require('os')));
+*/
